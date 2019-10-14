3520330518c2fea52ed848a450e694a8d4f5d2bf
jeremydmiller <jeremydmiller@yahoo.com>
2019-10-14 11:39:56 -0500

    initial import of the code

---
+++ src/HttpTests/Routing/RoutingApp.cs
@@ -0,0 +1,21 @@
+﻿using Jasper;
+using JasperHttp;
+using TestingSupport;
+
+namespace HttpTests.Routing
+{
+    public class RoutingApp : JasperRegistry
+    {
+        public RoutingApp()
+        {
+            Handlers.DisableConventionalDiscovery();
+
+            Settings.Http(x =>
+            {
+                x.DisableConventionalDiscovery()
+                    .IncludeType<SpreadHttpActions>()
+                    .IncludeType<RouteEndpoints>();
+            });
+        }
+    }
+}
---
+++ src/JasperHttp/HttpSerializationGraph.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Jasper.Conneg;
+using JasperHttp.ContentHandling;
+
+namespace JasperHttp
+{
+    public class HttpSerializationGraph : SerializationGraph<IRequestReader, IResponseWriter>
+    {
+        public HttpSerializationGraph(IEnumerable<ISerializerFactory<IRequestReader, IResponseWriter>> serializers,
+            IEnumerable<IRequestReader> readers, IEnumerable<IResponseWriter> writers) : base(serializers, readers,
+            writers)
+        {
+        }
+    }
+}
